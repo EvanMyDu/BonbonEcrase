a3f4b8bdc38e28de5730c0ad658e4ed557214f37
Minitruuck <gabriellaboureaup@gmail.com>
2024-11-20 10:58:45 +0100

    Installation librairie SDL

--- main/main.c
+++ main/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <SDL.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+
+
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, SDL_WINDOW_OPENGL);
+    SDL_Delay(5000);
+    SDL_Quit();
     return 0;
 }
