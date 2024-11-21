f81d56df1920a0133fdec046f0806bae64ca6291
Minitruuck <gabriellaboureaup@gmail.com>
2024-11-21 10:45:00 +0100

    Création fichier pour les graphismes et mettre la fenêtre en plein écran

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, SDL_WINDOW_OPENGL);
+    SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, SDL_WINDOW_FULLSCREEN_DESKTOP);
     SDL_Delay(5000);
     SDL_Quit();
     return 0;
