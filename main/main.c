a2707f91f1b9388bfefe234a177874a34936e001
Azeltor <evanmydu@gmail.com>
2024-11-26 22:43:08 +0100

    Boucle de Jeu V1

--- main/main.c
+++ main/main.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <SDL.h>
-
-
-
-
 int main(int argc, char* argv[]) {
+    SDL_bool run = SDL_TRUE;
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    SDL_Delay(5000);
+    SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, SDL_WINDOW_FULLSCREEN);
+    while(run){
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch(event.type)
+            {
+                case SDL_QUIT:
+                    run = SDL_FALSE;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     SDL_Quit();
     return 0;
 }
+
+
+
+
