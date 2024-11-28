d0975358c087ab52a0d441a7768c83eab781d388
Minitruuck <151580939+Minitruuck@users.noreply.github.com>
2024-11-28 23:09:53 +0100

    Installation de la librairie pour intégrer des images
    Il manque l'installation dans le CMake list

--- main/main.c
+++ main/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <SDL.h>
+#include <SDL_image.h>
 
 int main(int argc, char* argv[]) {
     SDL_bool run = SDL_TRUE, maximized = SDL_TRUE;
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1040, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Image* image = IMG_Load("image.png");
     while(run){
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -16,8 +19,12 @@ int main(int argc, char* argv[]) {
                 default:
                     break;
             }
+            SDL_SetRenderDrawColor(renderer, 100, 40, 40, 255);
+            SDL_RenderClear(renderer);
+            SDL_RenderPresent(renderer);
         }
     }
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
